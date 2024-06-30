e0809056947f3bf37c4a8dd47a0200effc93e023
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-06-30 18:09:15 +0000

    Update

---
+++ hotel_management_system/akaunting/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-name/hotel-management-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '74e36f9b5a2020896a31bf15655ac6ffec454098',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'your-name/hotel-management-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '74e36f9b5a2020896a31bf15655ac6ffec454098',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
